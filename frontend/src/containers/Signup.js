4885a2b5d1a64b7e458199ed028a880aafbab091
mon33k <moniquemojica@ac.c4q.nyc>
2019-04-29 13:09:30 -0400

    added comments inside of sign up component and refactored code

--- frontend/src/containers/Signup.js
+++ frontend/src/containers/Signup.js
@@ -1,6 +1,13 @@
+/*
+1) This component has an input form that accepts a username, fullname, email and password.
+
+2) With a handle submit function I am validating the user through firebase and then setting the registered user to the state, and then sending a POST request where I add that new user to my postGres tables. 
+
+3) Lastly inside of componentDidMount I am setting my localStorage to the firebase auth_token that will change with every new logged in session and delete after the user is logged out. 
+*/
+
 import React from 'react';
 import firebase from '../firebase'
-import Profile from './Profile'
 import axios from 'axios';
 
 
@@ -27,10 +34,9 @@ class Signup extends React.Component {
     handleSubmit = (e) => {
         e.preventDefault()
         const { email, password, displayName, fullname } = this.state
-        // This request validates the user via firebase
+        // This request validates the user via firebase and sets to state the user returned
         firebase.auth().createUserWithEmailAndPassword(email, password)
             .then((res) => {
-                // console.log('response', res, res.user.email, res.user.uid, displayName)
                 this.setState({
                     user: {
                         username: displayName,
@@ -38,8 +44,6 @@ class Signup extends React.Component {
                         uid: res.user.uid,
                     }
                 })
-                this.props.setCurrentUser(this.state.user)
-
                 return res
             })
             // This request POSTS the new user to the user table
@@ -51,14 +55,9 @@ class Signup extends React.Component {
                     firebase_uid: res.user.uid
                 })
                 .then((res) => {
-                    console.log('res success post request! ', res.data)
-
-                    // let currentUser = res.data[0]
-                    // this.props.setCurrentUser(currentUser)
-                    //This gave me a memory leak error and I don't know why 
-                    // this.setState({
-                    //     message: "user logged in success"
-                    // })
+                    console.log('res success post request on Sign up! ')
+                    // Passing the user as props back to App.js
+                    this.props.setCurrentUser(res.data.data[0])
                 })
                 .catch(err => {
                     console.log('Couldnt make a POST request ', err)
@@ -69,10 +68,6 @@ class Signup extends React.Component {
                     message: err.message
                 })
             })
-
-        // let user = firebase.auth().currentUser
-
-        // console.log('userrr', user)
     }
 
     componentDidMount() {
@@ -96,33 +91,13 @@ class Signup extends React.Component {
             .then((token) => {
                 return localStorage.setItem('auth_token', JSON.stringify(token))
             })
-        // let user = firebase.auth().currentUser
-        // if (user !== null) {
-        //     //Adding username
-        //     user.updateProfile({
-        //         displayName: this.state.displayName
-        //     })
-        //     //Adding current user info
-        //     user.providerData.forEach((user) => {
-        //         this.setState({
-        //             currentUser: {
-        //                 username: user.displayName,
-        //                 user_email: user.email,
-        //                 user_uid: user.uid
-        //             }
-        //         })
-        //     })
-
     }
 
 
 
     render() {
-        const { email, password, message, displayName, user, fullname } = this.state
+        const { email, password, message, displayName, fullname } = this.state
 
-        // if (user.email) {
-        //     return (<Profile user={user} />)
-        // } else {
             return (
                 <div>
                     <h1>Sign Up</h1>
@@ -145,7 +120,6 @@ class Signup extends React.Component {
                     {message ? <div>{message}</div> : ""}
                 </div>
             )
-        // }
     }
 }
 
